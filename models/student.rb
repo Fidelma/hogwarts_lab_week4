9389c03c6b702fdc1c425c00865873f520fc306c
Fidelma <fidelma.beagan@gmail.com>
2019-07-18 11:28:20 +0100

    student class and save method added

---
+++ models/student.rb
@@ -0,0 +1,28 @@
+require_relative('../db/sql_runner.rb')
+
+class Students
+
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :house, :age
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @house = options['house']
+    @age = options['age'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO students
+          (first_name,
+            last_name,
+            house,
+            age)
+            VALUES($1, $2, $3, $4)
+            RETURNING id"
+    values = [@first_name, @last_name, @house, @age]
+    @id = SqlRuner.run(sql, values)
+  end
+  
+end
